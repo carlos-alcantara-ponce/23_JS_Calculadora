513e6722b4d48104e25d22381c51da2bbc4f435d
Carlos.Ernesto.A <calcanta_peru@hotmail.com>
2023-06-17 10:05:05 -0500

    First Commit

--- script.js
+++ script.js
@@ -1,24 +1,71 @@
-function fFizzBuzz() {
+function fImpares() {
 
+
+    for (i = 1; i <= 20; i++) {
+
+        if (!(i % 2 === 0)) {
+            console.log(i);
+        }
+    }
+
+}
+
+
+function fMul3() {
+
+
+    for (i = 100; i > -1; i--) {
+
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+
+
+function fSecuen() {
+
+    for (i = 4; i >= -3.5; i += -1.5) {
+
+
+        console.log(i);
+
+    }
+
+}
+
+function fSigma() {
+    var sum = 0;
 
     for (i = 1; i <= 100; i++) {
 
-        if ((i % 3 === 0) && (i % 5 === 0)) {
 
-            console.log("FizzBuzz");
-        }
-        else if (i % 5 === 0) {
+        sum = sum + i;
 
-            console.log("Buzz");
 
-        }
-        else if (i % 3 === 0) {
-            console.log("Fizz");
-        }
-        else {
-            console.log(i);
-
-        }
     }
+
+    console.log(sum);
+
+
+}
+
+
+function fFactorial() {
+
+    var product = 1;
+
+    for (i = 1; i <= 12; i++) {
+
+
+        product = product * i;
+
+
+    }
+
+    console.log(product);
+
+
 
 }
